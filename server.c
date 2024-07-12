a10a65aaa52e414425f087b4619ad096ca260583
Kevin Liu <kevin.liu4@mail.mcgill.ca>
2024-07-11 23:50:51 -0400

    GET File and POST File

--- server.c
+++ server.c
@@ -13,8 +13,18 @@
 #define MAX_THREADS 10
 
 void *handle_connection(void* arg);
+char* g_directory;
 
-int main() {
+
+int main(int argc, char *argv[]) {
+	g_directory = "/tmp";
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--directory") == 0 && i + 1 < argc) {
+            g_directory = argv[i + 1];
+            break;
+        }
+    }
+
     // Disable output buffering
     setbuf(stdout, NULL);
     setbuf(stderr, NULL);
@@ -92,11 +102,20 @@ int main() {
 		// The thread will be automatically cleaned up when it finishes
 		pthread_detach(thread);
 	}
-	
+
     close(client_fd);
     close(server_fd);
 
     return 0;
+}
+
+int file_exists(char* path) {
+	FILE* file = fopen(path, "r");
+	if (file) {
+		fclose(file);
+		return 1;
+	}
+	return 0;
 }
 
 void *handle_connection(void *arg) {
@@ -125,52 +144,129 @@ void *handle_connection(void *arg) {
         printf("Path: %s\n", path);
     }
 
-    if (path) {	
-        char* pathForEcho = strtok(path, "/");
-        if (pathForEcho) {
-            echoContent = strtok(NULL, "/");
-        }
-    }
+	// Check if the method is GET
+	if (strcmp(method, "GET") == 0) {
+		if (strncmp(path, "/files", 6) == 0) {
+			char* filename = path + 6;  // Skip "/files/"
+			char filepath[4096];
+			snprintf(filepath, sizeof(filepath), "%s%s", g_directory, filename + 1);
+			
+			printf("File request\n");
+			printf("File path: %s\n", filepath);
 
-    if (strcmp(path, "/") == 0) {
-        char* response = "HTTP/1.1 200 OK\r\n\r\n";
-        send(client_fd, response, strlen(response), 0);
-    } else if (strcmp(path, "/echo") == 0) {
-        char bufferForEchoContent[4096];
-        snprintf(bufferForEchoContent, sizeof(bufferForEchoContent), "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %ld\r\n\r\n%s", strlen(echoContent), echoContent);
-        send(client_fd, bufferForEchoContent, strlen(bufferForEchoContent), 0);
-	} else if (strcmp(path, "/user-agent") == 0) {
+			FILE* file = fopen(filepath, "rb");
+			if (file) {
+				fseek(file, 0, SEEK_END);
+				long file_size = ftell(file);
+				fseek(file, 0, SEEK_SET);
 
-        char* userAgent = NULL;
+				char* file_content = malloc(file_size);
+				if (file_content == NULL) {
+					fclose(file);
+					close(client_fd);
+					return NULL;
+				}
 
-        while ((line = strtok_r(NULL, "\r\n", &saveptr)) != NULL) {
-            if (strncmp(line, "User-Agent:", 11) == 0) {
-                userAgent = line + 11;
+				fread(file_content, 1, file_size, file);
+				fclose(file);
 
-                while (*userAgent && isspace(*userAgent)) {
-                    userAgent++;
-                }
-                break;
-            }
-        }
+				char response_header[4096];
+				int header_length = snprintf(response_header, sizeof(response_header), 
+					"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %ld\r\n\r\n",
+					file_size);
 
-        if (userAgent) {
-            char response[4096];
-            int response_length = snprintf(response, sizeof(response), 
-                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %ld\r\n\r\n%s",
-                strlen(userAgent), userAgent);
-            send(client_fd, response, response_length, 0);
-        } else {
-            printf("User-Agent not found, sending 400 Bad Request\n");
-            char *response = "HTTP/1.1 400 Bad Request\r\n\r\nUser-Agent header not found";
-            send(client_fd, response, strlen(response), 0);
-        }
-    }
-	else {
-        char* response = "HTTP/1.1 404 Not Found\r\n\r\n";
-        send(client_fd, response, strlen(response), 0);
-    }
+				send(client_fd, response_header, header_length, 0);
+				send(client_fd, file_content, file_size, 0);				
 
+				free(file_content);
+			} else {
+				printf("File not found\n");
+				char* response = "HTTP/1.1 404 Not Found\r\n\r\n";
+				send(client_fd, response, strlen(response), 0);
+			}
+
+		} else if (strcmp(path, "/") == 0) {
+			char* response = "HTTP/1.1 200 OK\r\n\r\n";
+			send(client_fd, response, strlen(response), 0);
+		} else if (strncmp(path, "/echo", 5) == 0) {
+			
+			char* pathForEcho = strtok(path, "/");
+			if (pathForEcho) {
+				echoContent = strtok(NULL, "/");
+			}
+
+			char bufferForEchoContent[4096];
+			snprintf(bufferForEchoContent, sizeof(bufferForEchoContent), "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %ld\r\n\r\n%s", strlen(echoContent), echoContent);
+			send(client_fd, bufferForEchoContent, strlen(bufferForEchoContent), 0);
+		} else if (strcmp(path, "/user-agent") == 0) {
+			char* userAgent = NULL;
+			while ((line = strtok_r(NULL, "\r\n", &saveptr)) != NULL) {
+				if (strncmp(line, "User-Agent:", 11) == 0) {
+					userAgent = line + 11;
+
+					while (*userAgent && isspace(*userAgent)) {
+						userAgent++;
+					}
+					break;
+				}
+			}
+			if (userAgent) {
+				char response[4096];
+				int response_length = snprintf(response, sizeof(response), 
+					"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %ld\r\n\r\n%s",
+					strlen(userAgent), userAgent);
+				send(client_fd, response, response_length, 0);
+			} else {
+				printf("User-Agent not found, sending 400 Bad Request\n");
+				char *response = "HTTP/1.1 400 Bad Request\r\n\r\nUser-Agent header not found";
+				send(client_fd, response, strlen(response), 0);
+			}
+		}
+		else {
+			char* response = "HTTP/1.1 404 Not Found\r\n\r\n";
+			send(client_fd, response, strlen(response), 0);
+		}
+	} else if (strcmp(method, "POST") == 0) {
+		if (strncmp(path, "/files/", 7) == 0) {
+			printf("This is a POST request\n");
+			char* filename = path + 7;
+			char filepath[4096];
+			snprintf(filepath, sizeof(filepath), "%s/%s", g_directory, filename);
+			
+			char* contentType = NULL;
+			char* body = NULL;
+			while ((line = strtok_r(NULL, "\r\n", &saveptr)) != NULL) {
+				if (strncmp(line, "Content-Type:", 13) == 0) {
+					printf("Line: %s\n", line); // Content-Length: 5
+					contentType = line + 13;
+
+					while (*contentType && isspace(*contentType)) {
+						contentType++;
+					}
+				}
+			}
+			
+			body = contentType + 24 + 4; // Skip "application/octet-stream\r\n\r\n"
+			printf("Body: %s\n", body);
+
+
+			FILE* file = fopen(filepath, "wb");
+			if (file) {
+				fwrite(body, 1, strlen(body), file);
+				fclose(file);
+				
+				char* response = "HTTP/1.1 201 Created\r\n\r\n";
+				send(client_fd, response, strlen(response), 0);
+			} else {
+				printf("Failed to open file for writing\n");
+				char* response = "HTTP/1.1 404 Not Found\r\n\r\n";
+				send(client_fd, response, strlen(response), 0);
+			}
+		} else {
+			char* response = "HTTP/1.1 404 Not Found\r\n\r\n";
+			send(client_fd, response, strlen(response), 0);
+		}
+	}
 	close(client_fd);
 	return NULL;
 }
